5f640c4e68419723f3e1e15a1ca8f0ac030422d1
JoseAPR <92516246+Jose-Antonio96@users.noreply.github.com>
2023-10-04 13:59:20 +0200

    configuracion

---
+++ src/test/java/com/pedido/pedidoapi/PedidoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedido.pedidoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
